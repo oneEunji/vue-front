8f4dcb60087b6c78c987509c2ed9ab6d561b4c0e
oneEunji <eunjikimbusness@gmail.com>
2024-07-18 15:18:12 +0900

    test

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
     devServer: {
+        allowedHosts: "all",
         proxy: {
             '/api': {
                 target: 'http://localhost:3001',
                 changeOrigin: true,
+                ws:false,
                 pathRewrite: {
                     '^/api': ''
                 }
